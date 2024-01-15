0c52f6a23c417dd6dbb47781e72067f071716bf2
Pedro <pedro795027@gmail.com>
2024-01-15 15:53:23 -0300

    feat: criação dos temas a serem utilizados

---
+++ src/Styles/theme.ts
@@ -0,0 +1,18 @@
+import { extendTheme } from 'native-base';
+
+export const THEME = extendTheme ({
+    colors: {
+        gray: {
+            300: '#8D8D99'
+        },
+        blue: {
+            500: '#339CFF',
+            800: '#0B3B60'
+        },
+        white: '#FFF',
+        black: '#000'
+    },
+    fontSizes: {
+        
+    }
+})
